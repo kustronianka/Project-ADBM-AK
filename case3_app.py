deda3492f8f9899246d17425cae5c81ea0ce8430
kustronianka <kustron.ak@gmail.com>
2025-11-03 18:19:14 +0100

    sonsors bug fixed

--- case3_app.py
+++ case3_app.py
@@ -32,21 +32,45 @@ def read_file():
             return_datetime(df_json)
             return df_json
         
-def calculate():
+def find_sensor_col(case):
     global results
     df = read_file()
     columns = df.columns
+    if "sensor_id" in columns:
+        for column in columns:
+            if column == "sensor_id":
+                unique_names = df[column].unique()
+                for unique in unique_names:
+                    sorted = df[df[column] == unique]
+                    if case == "plot":
+                        plot_charts(sorted, unique)
+                    elif case == "calc":
+                        calculate(sorted, unique)   
+    else:
+        if case == "plot":
+            plot_charts(df, "sensor data")
+        elif case == "calc":
+            calculate(df, "sensor data")
+        
+def plot_button():
+    find_sensor_col("plot")
+
+def calc_button():
+    find_sensor_col("calc")
+
+def calculate(df, name):
+    global results
+    columns = df.columns
+    results["names"] = ("mean", "std", "val_min", "val_max")
     for column in columns:
         if is_numeric_dtype(df[column]):
             mean = round(df[column].mean(skipna=True), 2)
             std = round(df[column].std(ddof=1, skipna=True), 2)
             val_min = round(df[column].min(skipna=True), 2)
             val_max = round(df[column].max(skipna=True), 2)
-            msg = f" {column}: \n mean: {mean}, \n std: {std}, \n minimum: {val_min}, \n maximum: {val_max}"
+            msg = f" {name}, \n {column}: \n mean: {mean}, \n std: {std}, \n minimum: {val_min}, \n maximum: {val_max}"
             messagebox.showinfo("Results", msg)
-            results[column] = (mean, std, val_min, val_max)
-            results["names"] = ("mean", "std", "val_min", "val_max")
-    return results
+            results[name] = (mean, std, val_min, val_max)
 
 def save_to_csv():
     resluts_df = pd.DataFrame(results)
@@ -60,12 +84,10 @@ def find_col(df, name):
             return column
     return None
 
-def plot_charts():
+def plot_charts(df, name):
     date_min = combo_date_min.get()
     date_max = combo_date_max.get()
-    df = read_file()
     columns = df.columns
-    plt.close()
     fig, ax = plt.subplots()
     for column in columns:
         if is_numeric_dtype(df[column]):
@@ -73,6 +95,7 @@ def plot_charts():
             x = df[timestamp]
             y = df[column]
             ax.plot(x, y, label=column)
+            ax.set_title(name)
             ax.set_xlabel(timestamp)
             ax.set_ylabel(column)
             ax.grid(True, which='major', linestyle='--', alpha=0.4)
@@ -100,8 +123,8 @@ root.geometry("640x520")
 read_button = ttk.Button(root, text="Read file", command=get_path).pack(pady=6)
 frame_actions = ttk.Frame(root)
 frame_actions.pack(fill="x", pady=6)
-calculate_button = ttk.Button(frame_actions, text="calculate", command=calculate).pack()
-plot_button = ttk.Button(frame_actions, text="plot", command=plot_charts).pack()
+calculate_button = ttk.Button(frame_actions, text="calculate", command=calc_button).pack()
+plot_button = ttk.Button(frame_actions, text="plot", command=plot_button).pack()
 
 frame_filter = ttk.Frame(root)
 frame_filter.pack(fill="x")
